cb87278bc05c1c1b1986bb735bf41f394d02dac3
Rummansarker <rummansarker567@gmail.com>
2023-03-27 12:38:44 +0600

    added product style

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -7,10 +7,13 @@ const Product = (props) => {
   return (
     <div className='product'>
       <img src={img} alt="" />
-      <h5>{name}</h5>
-      <h6>Price: ${price}</h6>
-      <p>Manufacturer: {seller}</p>
-      <p>Ratings: {ratings}</p>
+      <div className='product-info'>
+        <h6 className='product-name'>{name}</h6>
+        <p>Price: ${price}</p>
+        <p>Manufacturer: {seller}</p>
+        <p>Ratings: {ratings} star</p>
+      </div>
+      <button className='btn-cart'>Add to Cart</button>
     </div>
   );
 };
