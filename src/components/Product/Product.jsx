ee29b1941ddc73ab247003d0429fbc4c8b8a9f9a
Rummansarker <rummansarker567@gmail.com>
2023-03-27 13:52:26 +0600

    added font awesome icon

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 
 const Product = (props) => {
-  console.log(props.product)
+  // console.log(props)
   const {name, img, seller, price, quantity, ratings} = props.product;
+  const handleAddToCart = props.handleAddToCart;
+
+
   return (
     <div className='product'>
       <img src={img} alt="" />
@@ -13,7 +18,10 @@ const Product = (props) => {
         <p>Manufacturer: {seller}</p>
         <p>Ratings: {ratings} star</p>
       </div>
-      <button className='btn-cart'>Add to Cart</button>
+      <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
+        Add to Cart
+        <FontAwesomeIcon icon={faShoppingCart} />
+        </button>
     </div>
   );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,13 +3,19 @@ import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect( () => {
     fetch('products.json')
     .then(res => res.json())
     .then(data => setProducts(data));
-  }, [])
+  }, []);
+
+  const handleAddToCart = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+  }
 
   return (
     <div className='shop-container'>
@@ -18,11 +24,13 @@ const Shop = () => {
           products.map(product => <Product
           key={product.id}
           product={product}
+          handleAddToCart={handleAddToCart}
           ></Product>)
         }
       </div>
       <div className="cart-container">
         <h4>Order Summery</h4>
+        <p>Selected Item: {cart.length}</p>
       </div>
     </div>
   );
